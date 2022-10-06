d3245d215104ec4cf09bd8cb98d4ae128754f44e
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-10-06 17:35:47 -0500

    Style css on hardcoded html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import css from './style.css';
 
-const content = document.getElementById('content');
-const test = document.createElement('h1');
-test.id = 'test';
-test.innerHTML= 'test';
-content.appendChild(test);
+
